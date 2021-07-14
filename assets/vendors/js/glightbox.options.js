489734099255e26f631205fb967078591d67b7e5
TopGitSan <c.toparcean@gmail.com>
2021-07-14 12:18:32 +0200

    add video play on button click using glightbox

---
+++ assets/vendors/js/glightbox.options.js
@@ -0,0 +1,9 @@
+const lightbox = GLightbox({
+  touchNavigation: true,
+  loop: true,
+  autoplayVideos: true,
+  href: "https://www.youtube.com/watch?v=-FCUQadyLzI",
+  type: "video",
+  source: "youtube", //vimeo, youtube or local
+  width: 900,
+});
